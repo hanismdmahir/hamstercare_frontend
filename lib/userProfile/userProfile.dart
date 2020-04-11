62162997e680da90efac3852809e67386757e030
AtiqahAqilah <atisyah.qatie@gmail.com>
2020-04-11 15:24:46 +0800

    Added Settings

--- lib/userProfile/userProfile.dart
+++ lib/userProfile/userProfile.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
+final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
 class ProfilePage extends StatefulWidget {
+
   @override
   _ProfilePageState createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
   int _currentIndex = 4;
+  bool _noti = false;
+  get noti => _noti;
+  set noti(value) => setState(() => _noti = value);
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +27,257 @@ class _ProfilePageState extends State<ProfilePage> {
 
    Drawer buildEndDrawer() {
     return Drawer(
-        //setting tiqah
-        );
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            padding: EdgeInsets.zero,
+            child: Container(
+              color: Colors.orange,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: CircleAvatar(
+                      radius: 50.0,
+                      backgroundColor: Colors.grey,
+                      backgroundImage: AssetImage('assets/photo4.jpg'),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      'user999',
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          //profile picture
+          ListTile(
+            leading: Icon(Icons.photo_camera),
+            title: Text('Profile Picture'),
+            trailing: Icon(Icons.edit),
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Profile Picture"),
+                      content: Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.photo_camera,
+                                size: 26,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.photo_library,
+                                size: 26,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: Text('Upload'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        FlatButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            }),
+                      ],
+                    );
+                  });
+            },
+          ),
+          Divider(),
+          //bio
+          ListTile(
+            leading: Icon(Icons.description),
+            title: Text('Bio'),
+            trailing: Icon(Icons.edit),
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Bio"),
+                      content: TextField(
+                        maxLength: 50,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'New Bio',
+                        ),
+                      ),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: Text('Save'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        FlatButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            }),
+                      ],
+                    );
+                  });
+            },
+          ),
+          Divider(),
+          //username
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Username'),
+            trailing: Icon(Icons.edit),
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Username"),
+                      content: TextField(
+                        maxLength: 10,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'New Username',
+                        ),
+                      ),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: Text('Save'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        FlatButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            }),
+                      ],
+                    );
+                  });
+            },
+          ),
+          Divider(),
+          //password
+          ListTile(
+            leading: Icon(Icons.lock),
+            title: Text('Password'),
+            trailing: Icon(Icons.edit),
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Password"),
+                      content: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'New Password',
+                        ),
+                      ),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: Text('Save'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        FlatButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            }),
+                      ],
+                    );
+                  });
+            },
+          ),
+          Divider(),
+          //email
+          ListTile(
+            leading: Icon(Icons.mail),
+            title: Text('Email'),
+            trailing: Icon(Icons.edit),
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Email"),
+                      content: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'New Email',
+                        ),
+                      ),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: Text('Save'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        FlatButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            }),
+                      ],
+                    );
+                  });
+            },
+          ),
+          Divider(),
+          //notification
+          SwitchListTile(
+            activeColor: Colors.orange,
+            value: noti,
+            onChanged: (value) => noti = value,
+            title: Text('Notification'),
+            secondary: Icon(Icons.notifications_active),
+          ),
+          Divider(),
+          //logout button
+          Container(
+            width: 100.0,
+            height: 40.0,
+            child: RaisedButton(
+              color: Colors.orange,
+              onPressed: () {},
+              child: Text(
+                'Logout',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+        ),
+      );
   }
 
   AppBar buildAppBar() {
@@ -35,8 +290,10 @@ class _ProfilePageState extends State<ProfilePage> {
           Text('HamsterCare'),
         ],
       ),
+      
     );
   }
+
 
   ListView buildListView() {
     return ListView(children: <Widget>[
